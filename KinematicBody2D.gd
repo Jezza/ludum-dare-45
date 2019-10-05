49789d5b6a51cdc9f746f17e1748b0a069bc20a2
NPException <npexception42@gmail.com>
2019-10-05 14:30:41 +0200

    player char

--- KinematicBody2D.gd
+++ KinematicBody2D.gd
@@ -1,18 +1,33 @@
 extends KinematicBody2D
 
+const GRAVITY = 60
+const TERMINAL_VELOCITY = 1000
+const ACCELERATION = 50
+const MAX_SPEED = 500
+const JUMP_HEIGHT = -1000
+const FRICTION = 0.2
+const DRAG = 0.05
+
 var motion = Vector2();
 
 func _physics_process(delta):
-	motion.y += 30;
+	motion.y = min(motion.y + GRAVITY, TERMINAL_VELOCITY)
+	var friction = true
 
-	if Input.is_action_pressed("ui_left"):
-		motion.x -= 6;
 	if Input.is_action_pressed("ui_right"):
-		motion.x += 6;
-	if is_on_floor() && Input.is_action_pressed("ui_up"):
-		motion.y -= 800;
+		motion.x = min(motion.x + ACCELERATION, MAX_SPEED)
+		friction = not friction
+	if Input.is_action_pressed("ui_left"):
+		motion.x = max(motion.x - ACCELERATION, -MAX_SPEED)
+		friction = not friction
 
 	if is_on_floor():
-		motion.x = lerp(motion.x, 0, 0.01);
+		if Input.is_action_just_pressed("ui_up"):
+			motion.y = JUMP_HEIGHT
+		if friction:
+			motion.x = lerp(motion.x, 0, FRICTION)
+	else:
+		if friction:
+			motion.x = lerp(motion.x, 0, DRAG)
 
-	motion = move_and_slide(motion, Vector2.UP);
+	motion = move_and_slide(motion, Vector2.UP)
